ece2c793b1e2de81adc58ec56e097afcbcc24f55
vanchuongkieu <vanchuongkieu@gmail.com>
2023-02-12 19:19:30 +0700

    Initial Commit

---
+++ vnex_server/wsgi.py
@@ -0,0 +1,25 @@
+"""
+WSGI config for vnex_server project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+import pathlib
+import dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vnex_server.settings')
+
+application = get_wsgi_application()
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+BASE_DIR = CURRENT_DIR.parent
+ENV_FILE_PATH = BASE_DIR / ".env"
+
+dotenv.load_dotenv(str(ENV_FILE_PATH))
+
